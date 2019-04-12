f4f72e84d1d45c9d5cbec83b036e5162d8e6b371
PCForeman <45657492+PCForeman@users.noreply.github.com>
2019-04-12 16:06:30 +0100

    12/04/2019

--- src/pages/modal-account/modal-account.ts
+++ src/pages/modal-account/modal-account.ts
@@ -1,8 +1,13 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavParams, ViewController } from 'ionic-angular';
-import { AngularFireDatabase } from 'angularfire2/database';
-import { AngularFireAuth } from 'angularfire2/auth';
-import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
+import { Component } from "@angular/core";
+import { AngularFireAuth } from "angularfire2/auth";
+import { AngularFireDatabase } from "angularfire2/database";
+import {
+  IonicPage,
+  NavParams,
+  ToastController,
+  ViewController,
+  LoadingController
+} from "ionic-angular";
 
 /**
  * Generated class for the ModalAccountPage page.
@@ -13,74 +18,103 @@ import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
 
 @IonicPage()
 @Component({
-  selector: 'page-modal-account',
-  templateUrl: 'modal-account.html',
+  selector: "page-modal-account",
+  templateUrl: "modal-account.html"
 })
 export class ModalAccountPage {
+  constructor(
+    public navParams: NavParams,
+    private loader:LoadingController,
+    private toast: ToastController,
+    private view: ViewController,
+    private afDatabase: AngularFireDatabase,
+    private afAuth: AngularFireAuth
+  ) {}
+  uSurname: any;
+  uName: any;
+  password: any;
+  uPC: any;
+  userD: any;
+  uPhoneNo: any;
+  uAddressL1: any;
 
-  constructor(public navParams: NavParams, private view: ViewController,
-    private afDatabase: AngularFireDatabase, private afAuth: AngularFireAuth) {
+  close() {
+    this.view.dismiss();
   }
-uSurname:any;
-uName:any;
-password:any;
-uPC:any;
-userD:any;
-uPhoneNo:any;
-uAddressL1:any;
 
-close(){
-  this.view.dismiss();
-}
+  showSpinner() {
+    let loading = this.loader.create({
+      content: ""
+    });
+    loading.present();
+    setTimeout(() => {
+      loading.dismiss();
+    }, 250);
+  }
 
-updateSurname(){
-  var key = this.afAuth.auth.currentUser.uid
-  var ref = this.afDatabase.object(`/user/${key}`)
-  ref.update({surname: this.uSurname});
-  console.log(ref);
-}
+  successMessage(){
+    this.toast.create({message:'Successfully updated record.', duration: 1000, position:'top'}).present()
+  }
 
-updateName(){
-  var key = this.afAuth.auth.currentUser.uid
-  var ref = this.afDatabase.object(`/user/${key}`)
-  ref.update({firstname: this.uName});
-  console.log(ref);
-}
+  updateSurname() {
+    this.showSpinner();
+    var key = this.afAuth.auth.currentUser.uid;
+    var ref = this.afDatabase.object(`/user/${key}`);
+    ref.update({ surname: this.uSurname });
+    console.log(ref);
+    this.successMessage();
+  }
 
-updateAddress(){
-  var key = this.afAuth.auth.currentUser.uid
-  var ref = this.afDatabase.object(`/user/${key}`)
-  ref.update({addressL1: this.uAddressL1});
-  console.log(ref);
-}
+  updateName() {
+    this.showSpinner();
+    var key = this.afAuth.auth.currentUser.uid;
+    var ref = this.afDatabase.object(`/user/${key}`);
+    ref.update({ firstname: this.uName });
+    console.log(ref);
+    this.successMessage();
+  }
 
-updatePhoneNo(){
-  var key = this.afAuth.auth.currentUser.uid
-  var ref = this.afDatabase.object(`/user/${key}`)
-  ref.update({phoneNo: this.uPhoneNo});
-  console.log(ref);
-}
+    updateAddress() {
+    this.showSpinner()
+    var key = this.afAuth.auth.currentUser.uid;
+    var ref = this.afDatabase.object(`/user/${key}`);
+    ref.update({ addressL1: this.uAddressL1 });
+    console.log(ref);
+    this.successMessage();
+  }
 
-updatePostCode(){
-  var key = this.afAuth.auth.currentUser.uid
-  var ref = this.afDatabase.object(`/user/${key}`)
-  ref.update({addressPC: this.uPC});
-  console.log(ref);
-}
+  updatePhoneNo() {
+    this.showSpinner();
+    var key = this.afAuth.auth.currentUser.uid;
+    var ref = this.afDatabase.object(`/user/${key}`);
+    ref.update({ phoneNo: this.uPhoneNo });
+    console.log(ref);
+    this.successMessage();
+  }
 
-updatePassword(){
-  var newPass = this.password.toString()
-  console.log(newPass);
-  var key = this.afAuth.auth.currentUser.uid;
-  var ref = this.afDatabase.object(`/user/${key}`)
-  ref.update({password: newPass});
-  this.afAuth.auth.currentUser.updatePassword(newPass);
-}
+  updatePostCode() {
+    this.showSpinner();
+    var key = this.afAuth.auth.currentUser.uid;
+    var ref = this.afDatabase.object(`/user/${key}`);
+    ref.update({ addressPC: this.uPC });
+    console.log(ref);
+    this.successMessage();
+  }
+
+  updatePassword() {
+    this.showSpinner();
+    var newPass = this.password.toString();
+    console.log(newPass);
+    var key = this.afAuth.auth.currentUser.uid;
+    var ref = this.afDatabase.object(`/user/${key}`);
+    ref.update({ password: newPass });
+    this.afAuth.auth.currentUser.updatePassword(newPass);
+    this.successMessage();
+  }
 
   ionViewWillLoad() {
-  const ticket = this.navParams.get("ticket")
-  this.userD = ticket;
-  console.log(this.userD)
+    const ticket = this.navParams.get("ticket");
+    this.userD = ticket;
+    console.log(this.userD);
   }
-
 }
